477fbfa8938fdd79186da881fa9f1d7146181dfa
Ntipa Chola <sword@DESKTOP-NT5NC6Q>
2023-04-03 11:30:58 +0200

    Added upadated_at in BaseModel

--- techsupport/models.py
+++ techsupport/models.py
@@ -11,6 +11,7 @@ class BaseModel(models.Model):
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name=_("date created"))
+    updated_at = models.DateTimeField(auto_now=True, verbose_name=_("date modified"))
 
     class Meta:
         abstract = True
