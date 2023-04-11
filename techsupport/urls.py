ad20f4f66d8dd206624cbb83923ff58586c0016e
Dabwitso <mweembadabwitso@gmail.com>
2023-04-11 17:36:39 +0200

    new views

--- techsupport/urls.py
+++ techsupport/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import *
+from . import views
 
 urlpatterns = [
-    path("", HomePageView.as_view(), name="home"),
+    path("", views.HomePageView.as_view(), name="home"),
+    path("accounts/profile/", views.profile, name="profile"),
+    path("accounts/base/", views.base, name="base"),
+    path("accounts/login/", views.CustomLoginView.as_view(), name="account_login"),
+    path("support_ticket/create/", views.create_ticket, name="create_ticket"),
+    path("support_ticket/<int:ticket_id>/", views.ticket_detail, name="ticket_detail"),
+    path("support_ticket/<int:pk>/update/", views.update_ticket, name="update_ticket"),
+    path("support_ticket/list/", views.ticket_list, name="ticket_list"),
+    path("support_ticket/<int:ticket_id>/edit/", views.edit_ticket, name="edit_ticket"),
+    
 ]
