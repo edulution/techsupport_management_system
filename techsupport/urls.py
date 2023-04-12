217917a5d65f00b2d8be2fd7390ae0838ff23893
Dabwitso <mweembadabwitso@gmail.com>
2023-04-12 12:35:42 +0200

    Update urls.py with new views for support ticket management

--- techsupport/urls.py
+++ techsupport/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
@@ -8,9 +8,19 @@ urlpatterns = [
     path("accounts/base/", views.base, name="base"),
     path("accounts/login/", views.CustomLoginView.as_view(), name="account_login"),
     path("support_ticket/create/", views.create_ticket, name="create_ticket"),
-    path("support_ticket/<int:ticket_id>/", views.ticket_detail, name="ticket_detail"),
-    path("support_ticket/<int:pk>/update/", views.update_ticket, name="update_ticket"),
+    path("support_ticket/<int:ticket_id>/",
+         views.ticket_detail, name="ticket_detail"),
+    path("support_ticket/<int:pk>/update/",
+         views.update_ticket, name="update_ticket"),
     path("support_ticket/list/", views.ticket_list, name="ticket_list"),
-    path("support_ticket/<int:ticket_id>/edit/", views.edit_ticket, name="edit_ticket"),
-    
+    path("support_ticket/<int:ticket_id>/edit/",
+         views.edit_ticket, name="edit_ticket"),
+    path("support_ticket/admin_dashboard/",
+         views.admin_dashboard, name="admin_dashboard"),
+    path("support_ticket/coach_ticket_detail/<int:pk>/",
+         views.CoachTicketDetailView.as_view(), name="coach_ticket_detail"),
+    path("support_ticket/manager_dashboard/",
+         views.manager_dashboard, name="manager_dashboard"),
+    path("support_ticket/coach_ticket_list/",
+         views.CoachTicketListView.as_view(), name="coach_ticket_list"),
 ]
