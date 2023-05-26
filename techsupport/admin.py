ab1ef5ffbe5de37680f9c6926a36b152dfbd54c8
Kapya Sakala <kapsakala@gmail.com>
2023-05-26 10:23:53 +0200

    registered models on admin site

--- techsupport/admin.py
+++ techsupport/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
+from .models import User, Country, Centre, Category, SubCategory, SubCategory
 
-# from allauth.account.adapter import get_adapter
-# from allauth.account.admin import EmailAddressAdmin
-# from allauth.account.models import EmailAddress
-# from allauth.socialaccount.admin import SocialAccountAdmin
-# from allauth.socialaccount.models import SocialAccount, SocialApp, SocialToken
+admin.site.register(User)
+admin.site.register(Country)
+admin.site.register(Centre)
+admin.site.register(Category)
+admin.site.register(SubCategory)
