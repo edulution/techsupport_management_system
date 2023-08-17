8202b3f7002a8aee75927cd5545d28b7e7744745
Jeenyhus <mweembadabwitso@gmail.com>
2023-08-17 16:58:41 +0200

    support ticket styling

--- techsupport/templatetags/custom_filters.py
+++ techsupport/templatetags/custom_filters.py
@@ -6,3 +6,16 @@ register = template.Library()
 def cycle_color(value):
     colors = ['primary', 'secondary', 'success', 'danger', 'warning', 'info']
     return colors[value % len(colors)]
+
+@register.filter
+def add_placeholder(field, placeholder_text):
+    field.field.widget.attrs.update({'placeholder': placeholder_text})
+    return field
+
+@register.filter
+def add_class(field, class_name):
+    if 'class' in field.field.widget.attrs:
+        field.field.widget.attrs['class'] += f' {class_name}'
+    else:
+        field.field.widget.attrs['class'] = class_name
+    return field
