72b2fd2b3fc8f2c1bca38f9773200d011f3d0623
Kapya Sakala <kapsakala@gmail.com>
2023-09-11 14:40:35 +0200

    added urls for email-based password reset

--- techsupport_management/urls.py
+++ techsupport_management/urls.py
@@ -16,11 +16,41 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from techsupport.views import user_login, user_logout
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/login/", user_login, name="login"),
-    path("accounts/logout/", user_logout, name="logout"),  # Add this line for logout
+    path("accounts/logout/", user_logout, name="logout"),
+    path(
+        "password_reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="accounts/password_reset.html",
+            email_template_name="accounts/password_reset_email.html",
+        ),
+        name="password_reset",
+    ),
+    path(
+        "password_reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="accounts/password_reset_done.html"
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="accounts/password_reset_confirm.html"
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="accounts/password_reset_complete.html"
+        ),
+        name="password_reset_complete",
+    ),
     path("", include("techsupport.urls")),
-    path('chaining/', include('smart_selects.urls')),
+    path("chaining/", include("smart_selects.urls")),
 ]
