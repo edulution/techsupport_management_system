eda9cf3994808f613a71f12512fb6e37f3565ea3
Kapya Sakala <kapsakala@gmail.com>
2023-09-19 14:29:58 +0200

    remove payslip issue from subcategories

--- techsupport/management/commands/create_categories_subcategories.py
+++ techsupport/management/commands/create_categories_subcategories.py
@@ -3,6 +3,7 @@ from termcolor import colored
 from django.core.management.base import BaseCommand
 from techsupport.models import Category, SubCategory
 
+
 class Command(BaseCommand):
     help = "Generates sample data for the tech support ticket system."
 
@@ -14,23 +15,39 @@ class Command(BaseCommand):
         ]
 
         subcategory_data = {
-            "Hardware": ["Tablet Issues", "Laptop Issues", "Router Issues", "Headphones Issues", "Solar Kit Issues", "General Power Issues", "Other"],
-            "Software": ["Google Drive Issues", "Kolibri Issues", "Tests/Assessments Issues", "Assign/Enroll Learners", "Delete/Insert Learners", "Classlist Issues", "Other"],
-            "Operations": ["Payslip Issues", "Stock Sheet Issues", "Other"],
+            "Hardware": [
+                "Tablet Issues",
+                "Laptop Issues",
+                "Router Issues",
+                "Headphones Issues",
+                "Solar Kit Issues",
+                "General Power Issues",
+                "Other",
+            ],
+            "Software": [
+                "Google Drive Issues",
+                "Kolibri Issues",
+                "Tests/Assessments Issues",
+                "Assign/Enroll Learners",
+                "Delete/Insert Learners",
+                "Classlist Issues",
+                "Other",
+            ],
+            "Operations": ["Stock Sheet Issues", "Other"],
         }
 
         try:
             for category_info in category_data:
                 category = Category.objects.create(
-                    name=category_info["name"],
-                    code=category_info["code"]
+                    name=category_info["name"], code=category_info["code"]
                 )
                 subcategories = subcategory_data.get(category_info["name"], [])
                 for subcategory_name in subcategories:
-                    SubCategory.objects.create(
-                        name=subcategory_name,
-                        category=category
-                    )
-            self.stdout.write(self.style.SUCCESS("Technical Support Management Categories and Subcategories created successfully!"))
+                    SubCategory.objects.create(name=subcategory_name, category=category)
+            self.stdout.write(
+                self.style.SUCCESS(
+                    "Technical Support Management Categories and Subcategories created successfully!"
+                )
+            )
         except Exception as e:
             self.stderr.write(self.style.ERROR(f"An error occurred: {e}"))
